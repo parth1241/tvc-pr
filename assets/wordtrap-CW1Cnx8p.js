7583868e0e6dea38e55f946e119ea61f4c2ed81d
Parth Karan <parthkaran@Parths-MacBook-Pro-904.local>
2026-01-31 17:40:10 +0530

    Updates

---
+++ assets/wordtrap-CW1Cnx8p.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{G as e}from"./Game-CSBGkSeg.js";window.addEventListener("load",()=>{new e});
--- assets/wordtrap-CfqvC9xJ.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{G as e}from"./Game-7nc8AzPk.js";window.addEventListener("load",()=>{new e});
